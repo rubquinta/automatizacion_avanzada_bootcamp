b12979f0dc110e34c575a69c4c2fb189aecb2552
Matias Rojas <Matias.Rojas@tsoftglobal.com>
2021-05-11 15:29:39 -0400

    Modulo Vuelo Ok, Test operativos - se cambio POM para hacer test maven.

--- src/test/java/automationcraft/testcreation/jetsmartG2/steps/Steps_ModuloVueloBajaG2.java
+++ src/test/java/automationcraft/testcreation/jetsmartG2/steps/Steps_ModuloVueloBajaG2.java
@@ -16,50 +16,57 @@ public class Steps_ModuloVueloBajaG2 {
 
     @Given("Que soy un usuario e ingreso a la web <JetSmart>")
     public void que_soy_un_usuario_e_ingreso_a_la_web() {
-        System.out.println("Ingreso a la web");
         jsHomePage.goToUrl("https://jetsmart.com/cl/es/");
-        System.out.println("Cierro PopUp");
         jsHomePage.cerrarModuloSuscribete();
     }
-
     @When("En el formulario ingreso origen <Santiago>")
-    public void en_el_formulario_ingreso_origen() {
+    public void en_el_formulario_ingreso_origen(){
         jsHomePage.seleccionVueloOrigenSantiago();
     }
-
     @And("Selecciono destino <Buenos Aires>")
     public void selecciono_destino() {
         jsHomePage.seleccionVueloDestinoArgentina();
     }
-
-    @And("Ingreso fecha de ida y vuelta")
-    public void ingreso_fecha_de_ida_y_vuelta() {
-        jsHomePage.fechasVuelo();
-    }
-
     @And("Selecciono destino <Arica>")
     public void selecciono_destino_arica() {
         jsHomePage.seleccionVueloDestinoArica();
     }
-
+    @And("Ingreso fecha de ida y vuelta")
+    public void ingreso_fecha_de_ida_y_vuelta() {
+        jsHomePage.fechasVuelo();
+    }
     @And("Presiono en el boton buscar")
     public void presiono_en_el_boton_buscar() {
         jsHomePage.buscarVuelo();
     }
-
-    @And("Presiono en continuar, sin seleccionar vuelos.")
-    public void presiono_en_continuar_sin_seleccionar_vuelos() {
+    @And("Selecciono mis vuelos")
+    public void selecciono_mis_vuelos() {
+        jsVuelos.llenaVuelos();
+    }
+    @And("Presiono en continuar")
+    public void presiono_en_continuar() {
         jsVuelos.continuar();
     }
 
+    @And("Ingreso datos de pasajero sin fecha de nac.")
+    public void ingreso_datos_de_pasajero_sin_fecha_de_nac() {
+        jsVuelos.ingresaDatosPasajero();
+    }
+    @And("Hacemos click en continuar")
+    public void hacemos_click_en_continuar() {
+        jsVuelos.continuar2();
+    }
     @Then("App muestra mensaje de alerta")
     public void app_muestra_mensaje_de_alerta() {
         Assert.assertEquals("Si Funciona el Alert por falta de fecha", jsHomePage.sinFecha());
     }
-
     @Then("App muestra mensaje de error")
     public void app_muestra_mensaje_de_error() {
         Assert.assertEquals("Caso existoso, debes presionar los vuelos",jsVuelos.msjError());
+    }
+    @Then("App muestra alerta por datos faltantes")
+    public void app_muestra_alerta_por_datos_faltantes() {
+        Assert.assertEquals("Caso existoso, debes completar todos los campos",jsVuelos.msjError2());
     }
 
 
